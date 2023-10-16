e2adb6f5e9f44620d98e06fe52f2363d162e08a6
Vibhuti Gudaghe <99797163+vibhutig001@users.noreply.github.com>
2023-10-16 12:14:01 +0530

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//Accept 2values from user 
+int main()
+{
+    printf("...........................\n");
+    printf("\tGAY GANESHH\n");
+    printf("...........................\n");
+   
+    return 0;
+}
---
+++ Program2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//accept 2 values from user and perform
+int main()
+{
+    int i,j,k;
+    printf("Enter the no 1:\n");
+    scanf("%d",&i);
+    printf("Enter the no 2:\n");
+    scanf("%d",&j);
+    k=i+j;
+    printf("addition is : %d\n",k);
+
+    return 0;
+}
